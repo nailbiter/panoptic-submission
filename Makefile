1c96704e6beeb1ba0f3e9d8e044372eaa5b42db7
Alex Leontiev <alozz1991@gmall.com>
2019-01-31 18:14:37 +0900

    Elix code files changed:  M Makefile  M coco/annotations/.gitignore AM pl/converter.pl
    trello card: https://trello.com/c/jf2kmDo0

--- Makefile
+++ Makefile
@@ -1,4 +1,4 @@
-.PHONY: all test convert_json docs setup
+.PHONY: all test convert_json docs setup test2
 
 #commands
 PYTHON=~/anaconda3/bin/python3
@@ -8,9 +8,12 @@ SEMANTIC_SEGMENTATION_JSON=./coco/annotations/semantic_segmentation.json
 PANOPTIC2SEMANTIC_SEGMENTATION=./converters/panoptic2semantic_segmentation.py
 INPUT_JSON_FILE=./coco/annotations/panoptic_train2017.json
 CATEGORIES_JSON_FILE=./coco/annotations/panoptic_coco_categories.json
+SEMANTIC_SEG_FOLDER=coco/annotations/semantic_segmentation_pngs
+CLASS_DICT=coco/annotations/class_dict.csv
 
 #phony target rules
-all: test
+all: test2
+test2: $(CLASS_DICT)
 test:
 	./pl/view_json.pl \
 		--log log/view_json.log.txt\
@@ -23,9 +26,17 @@ setup:
 
 #universal rules
 #non-universal rules
+$(CLASS_DICT): $(CATEGORIES_JSON_FILE) pl/converter.pl
+	./pl/converter.pl --in $< --mode SEGTOCSV > $@
 $(SEMANTIC_SEGMENTATION_JSON): 
 	$(PYTHON2) $(PANOPTIC2SEMANTIC_SEGMENTATION)\
 		--input_json_file $(INPUT_JSON_FILE)\
 		--output_json_file $(SEMANTIC_SEGMENTATION_JSON) \
+		--categories_json_file $(CATEGORIES_JSON_FILE)\
+		2>log/$(notdir $(basename $(SEMANTIC_SEGMENTATION_JSON))).log.txt
+	mkdir -p $(SEMANTIC_SEG_FOLDER)
+	$(PYTHON2) $(PANOPTIC2SEMANTIC_SEGMENTATION)\
+		--input_json_file $(INPUT_JSON_FILE)\
+	  	--semantic_seg_folder $(SEMANTIC_SEG_FOLDER)\
 		--categories_json_file $(CATEGORIES_JSON_FILE)\
 		2>log/$(notdir $(basename $(SEMANTIC_SEGMENTATION_JSON))).log.txt
---
+++ pl/converter.pl
@@ -0,0 +1,78 @@
+#!/usr/bin/env perl 
+#===============================================================================
+#
+#         FILE: converter.pl
+#
+#        USAGE: ./converter.pl  
+#
+#  DESCRIPTION: 
+#
+#      OPTIONS: ---
+# REQUIREMENTS: ---
+#         BUGS: ---
+#        NOTES: ---
+#       AUTHOR: YOUR NAME (), 
+# ORGANIZATION: 
+#      VERSION: 1.0
+#      CREATED: 01/31/19 17:54:55
+#     REVISION: ---
+#===============================================================================
+
+use strict;
+use warnings;
+use utf8;
+use Getopt::Long;
+use Data::Dumper;
+use JSON::Parse 'parse_json';
+
+
+#global const's
+my %CONVERTERS = (
+	SEGTOCSV=> '',
+);
+#procedures
+sub parseCommandLine{
+	(my $cmdline, my @arguments) = @_;
+	my %args;
+	for(@arguments){
+		$args{$_.'=s'} = \$$cmdline{$_};
+	}
+	GetOptions(
+		%args,
+	);
+}
+$CONVERTERS{SEGTOCSV} = sub {
+	my %cmdline = @_;
+#	printf(STDERR "%s\n",Dumper(\%cmdline));
+	my $json = loadJsonFromFile($cmdline{in});
+	printf("name,r,g,b\n");
+	for(@$json){
+#		printf("%s\n",Dumper($_));
+		printf("%s,%d, %d, %d\n",
+			$_->{name},
+			$_->{color}->[0],
+			$_->{color}->[1],
+			$_->{color}->[2]
+		);
+	}
+};
+sub loadJsonFromFile{
+	(my $filename) = @_;
+	open my $fh, '<', $filename or 
+		die sprintf("error opening %s\nerror=%s\n",$filename,$!);
+	my $data = do { local $/; <$fh> };
+	my $json = parse_json($data);
+	close($fh);
+	return $json;
+}
+
+#main
+my %cmdline;
+parseCommandLine(\%cmdline,'in','mode');
+for(keys %CONVERTERS){
+	if( $_ eq $cmdline{mode} ) {
+		$CONVERTERS{$_}->(%cmdline);
+		exit;
+	}
+}
+printf(STDERR "unknown converter \"%s\"\n",$cmdline{mode});
